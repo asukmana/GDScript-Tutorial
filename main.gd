7f1b70b209882d4e296c5a62bb3fbb146804895c
asukmana <128885541+asukmana@users.noreply.github.com>
2024-10-16 11:35:01 +0700

    Change text and text color in a label using code

--- main.gd
+++ main.gd
@@ -4,8 +4,8 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Hello world")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	$Label.text = "Hello world too"
+	#$Label.horizontal_alignment = INLINE_ALIGNMENT_CENTER
+	#$Label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	$Label.modulate = Color.ORANGE_RED
+	#$Label.add_theme_font_size_override("16",90)
