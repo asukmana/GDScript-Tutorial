d80819132940ec560089ae77f07b0b04ffc2885b
asukmana <128885541+asukmana@users.noreply.github.com>
2024-10-17 11:11:39 +0700

    Create function to concatenate text

--- main.gd
+++ main.gd
@@ -1,7 +1,15 @@
 extends Node
 
-var health = 100
+# script level variable
+# explicit data type
+var health: int = 100 #var health = 100 -> implicit data type 
+var player_name: String = "John Doe"
+var pi: float = 3.14
+var is_active: bool = true
 
+const GRAVITY = 9.8 #value cannot change at run time
+
+# underscore in front of function name -> function for internal use
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#print text in console
@@ -19,9 +27,15 @@ func _input(event):
 		$Label.modulate = Color.DARK_RED
 		if health > 0:
 			health -= 10
-			$Label.text = str(health)
+			# function level variable 
+			var sentence = reduce_health(player_name, str(health)) #str -> change integer to text
+			$Label.text = sentence 
 		else:
 			$Label.text = "Game Over"
 		
 	if event.is_action_released("my_action"):
 		$Label.modulate = Color.DARK_BLUE
+
+func reduce_health(string1: String, string2: String) -> String:
+	var sentence = string1 + " health is " + string2
+	return sentence
