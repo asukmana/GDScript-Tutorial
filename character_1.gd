f421205c26fc073f1489c9353a32c0c383912c58
asukmana <128885541+asukmana@users.noreply.github.com>
2024-10-21 11:09:43 +0700

    Create and instantiate inner class

--- character_1.gd
+++ character_1.gd
@@ -5,13 +5,24 @@ extends Node
 @export var profession : String
 @export var health : int
 
+#create instance of equipment class
+var chest := Equipment.new()
+var legs := Equipment.new()
+
+#inner class
+class Equipment:
+	var armor := 10
+	var weight := 5
+
 func die() :
 	health = 0
 	print (profession + " died.")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	chest.armor = 20
+	print(chest.armor)
+	print(legs.weight)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
